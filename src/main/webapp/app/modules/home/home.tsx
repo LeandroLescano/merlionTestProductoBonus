d1be0dd1c441bd73f17a9f23a654db8d3e7f32aa
LeanLescano <tatilescano11@gmail.com>
2020-08-22 01:05:38 -0300

    Home design

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -1,10 +1,20 @@
 import './home.scss';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Translate } from 'react-jhipster';
 import { connect } from 'react-redux';
 import { Row, Col, Alert } from 'reactstrap';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Container from '@material-ui/core/Container';
 
 import { IRootState } from 'app/shared/reducers';
 
@@ -12,90 +22,122 @@ export type IHomeProp = StateProps;
 
 export const Home = (props: IHomeProp) => {
   const { account } = props;
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    let token = '';
+    axios({ url: window.location.href + 'api/authenticate' })
+      .then(response => (token = response.config.headers.Authorization))
+      .catch(error => console.error(error));
+    const apiUrl = window.location.href + 'api/product-buckets';
+    if (mounted) {
+      axios({
+        url: apiUrl,
+        method: 'GET',
+        headers: {
+          accept: '*/*',
+          Authorization: token,
+        },
+      })
+        .then(response => setProductList(response.data.sort((a, b) => a.product.id - b.product.id)))
+        .catch(error => {
+          console.error(error);
+        });
+    }
+    return () => (mounted = false);
+  }, []);
+
+  const useStyles = makeStyles({
+    root: {
+      backgroundColor: '#2a6a9e',
+      color: 'lightgray',
+    },
+    tableHeader: {
+      backgroundColor: '#2a6a9e',
+    },
+    tableCell: {
+      color: 'white',
+    },
+    button: {
+      backgroundColor: '#2a6a9e',
+      color: 'white',
+      '&:hover': {
+        backgroundColor: '#5E99C5',
+      },
+    },
+  });
+
+  const styles = useStyles(props);
+
+  const showModal = product => {};
 
   return (
-    <Row>
-      <Col md="9">
-        <h2>
-          <Translate contentKey="home.title">Welcome, Java Hipster!</Translate>
-        </h2>
-        <p className="lead">
-          <Translate contentKey="home.subtitle">This is your homepage</Translate>
-        </p>
-        {account && account.login ? (
-          <div>
-            <Alert color="success">
-              <Translate contentKey="home.logged.message" interpolate={{ username: account.login }}>
-                You are logged in as user {account.login}.
-              </Translate>
-            </Alert>
-          </div>
-        ) : (
-          <div>
-            <Alert color="warning">
-              <Translate contentKey="global.messages.info.authenticated.prefix">If you want to </Translate>
-              <Link to="/login" className="alert-link">
-                <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
-              </Link>
-              <Translate contentKey="global.messages.info.authenticated.suffix">
-                , you can try the default accounts:
-                <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
-                <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
-              </Translate>
-            </Alert>
+    <Container>
+      {account && account.login ? (
+        <Paper>
+          <Table>
+            <TableHead>
+              <TableRow className={styles.tableHeader}>
+                <TableCell align="center" className={styles.tableCell}>
+                  Código
+                </TableCell>
+                <TableCell className={styles.tableCell}>Producto</TableCell>
+                <TableCell align="center" className={styles.tableCell}>
+                  Disponibles para la venta
+                </TableCell>
+                <TableCell align="center" className={styles.tableCell}>
+                  Encargados
+                </TableCell>
+                <TableCell align="center" className={styles.tableCell}>
+                  Defectuosos
+                </TableCell>
+                <TableCell className={styles.tableCell}></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {productList.map((prod, i) => {
+                return (
+                  <TableRow key={i}>
+                    <TableCell align="center">{prod.product.id}</TableCell>
+                    <TableCell>{prod.product.name}</TableCell>
+                    <TableCell align="center">{prod.availableToSellQuantity}</TableCell>
+                    <TableCell align="center">{prod.inChargeQuantity}</TableCell>
+                    <TableCell align="center">{prod.brokenQuantity}</TableCell>
+                    <TableCell align="center">
+                      <Button className={`${styles.button}`} onClick={prod => showModal(prod)}>
+                        Modificar
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </Paper>
+      ) : (
+        <div>
+          <Alert color="warning">
+            <Translate contentKey="global.messages.info.authenticated.prefix">If you want to </Translate>
+            <Link to="/login" className="alert-link">
+              <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
+            </Link>
+            <Translate contentKey="global.messages.info.authenticated.suffix">
+              , you can try the default accounts:
+              <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
+              <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
+            </Translate>
+          </Alert>
 
-            <Alert color="warning">
-              <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>&nbsp;
-              <Link to="/account/register" className="alert-link">
-                <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
-              </Link>
-            </Alert>
-          </div>
-        )}
-        <p>
-          <Translate contentKey="home.question">If you have any question on JHipster:</Translate>
-        </p>
-
-        <ul>
-          <li>
-            <a href="https://www.jhipster.tech/" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.homepage">JHipster homepage</Translate>
-            </a>
-          </li>
-          <li>
-            <a href="http://stackoverflow.com/tags/jhipster/info" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.stackoverflow">JHipster on Stack Overflow</Translate>
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/jhipster/generator-jhipster/issues?state=open" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.bugtracker">JHipster bug tracker</Translate>
-            </a>
-          </li>
-          <li>
-            <a href="https://gitter.im/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.chat">JHipster public chat room</Translate>
-            </a>
-          </li>
-          <li>
-            <a href="https://twitter.com/jhipster" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.follow">follow @jhipster on Twitter</Translate>
-            </a>
-          </li>
-        </ul>
-
-        <p>
-          <Translate contentKey="home.like">If you like JHipster, do not forget to give us a star on</Translate>{' '}
-          <a href="https://github.com/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-            Github
-          </a>
-          !
-        </p>
-      </Col>
-      <Col md="3" className="pad">
-        <span className="hipster rounded" />
-      </Col>
-    </Row>
+          <Alert color="warning">
+            <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>&nbsp;
+            <Link to="/account/register" className="alert-link">
+              <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
+            </Link>
+          </Alert>
+        </div>
+      )}
+    </Container>
   );
 };
 
